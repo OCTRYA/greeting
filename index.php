5cc376a0781c7eb7410d7a16cb4ed59dc93cccbd
Christian Toisoul <christian.toisoul@gharial.be>
2023-08-03 10:31:29 +0200

    second git action

--- index.php
+++ index.php
@@ -36,7 +36,11 @@ $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 echo $OUTPUT->header();
-echo '<h3>Greetings, user</h3>';
+if (isloggedin()) {
+    echo '<h3>Greetings, ' . fullname($USER) . '</h3>';
+} else {
+    echo '<h3>Greetings, user</h3>';
+}
 
 
 
